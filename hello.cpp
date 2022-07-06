f8c69973404c5ab08e3fe88fe5eb8c939938a6d0
MDostalek <dostalek.michal@atngmbh.de>
2022-07-06 11:36:21 +0200

    Test

--- hello.cpp
+++ hello.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
-#include "greeting.hpp"
 #include <cstdlib>
+
+#include "greeting.hpp"
+#include "integer.hpp"
 
 int main()
 {
+    auto integers = {1, 2, 3, 4, 5};
+    std::cout << sum_integers(integers) << std::endl;
     say_hello();
     std::cout << "Hello, World!" << std::endl;
     return 0;
---
+++ integer.cpp
@@ -0,0 +1,9 @@
+#include "integer.hpp"
+
+int sum_integers(const std::vector<int> integers) {
+    auto sum = 0;
+    for (auto i : integers) {
+        sum += i;
+    }
+    return sum;
+}
