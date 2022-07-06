574e8678c78963932b00a263edf853b5954cf5dd
MDostalek <dostalek.michal@atngmbh.de>
2022-07-06 11:45:12 +0200

    Main added

--- test/test.cpp
+++ test/test.cpp
@@ -2,10 +2,12 @@
 
 #include <vector>
 
-int sum_integers(const std::vector<int> integers) {
-  auto sum = 0;
-  for (auto i : integers) {
-    sum += i;
-  }
-  return sum;
+int main() {
+  auto integers = {1, 2, 3, 4, 5};
+
+    if (sum_integers(integers) == 15) {
+        return 0;
+    } else {
+        return 1;
+    }
 }
